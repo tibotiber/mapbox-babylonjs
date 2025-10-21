00b796a1803ad40a6247e7ecfee0f9da42916da1
Thibaut Tiberghien <thibaut@smplrspace.com>
2025-10-21 10:54:19 +0800

    add mapbox

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,25 @@
+import "mapbox-gl/dist/mapbox-gl.css";
+import mapboxgl from "mapbox-gl";
+
 import "./style.css";
 
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
+
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <h1>Coming soon</h1>
-  </div>
+  <div id="map"></div>
 `;
+
+const map = new mapboxgl.Map({
+  container: "map",
+  // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+  style: "mapbox://styles/mapbox/standard",
+  config: {
+    basemap: {
+      theme: "monochrome",
+    },
+  },
+  zoom: 18,
+  center: [148.9819, -35.3981],
+  pitch: 60,
+  antialias: true, // create the gl context with MSAA antialiasing, so custom layers are antialiased
+});
